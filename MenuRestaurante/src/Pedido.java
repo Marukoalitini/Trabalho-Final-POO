88489f5d9060611ca01ce09e521842eef3b2fdbf
Marco Agostini <107429726+Marukoalitini@users.noreply.github.com>
2024-10-30 19:52:09 -0300

    Criar Mesa e Pedido

---
+++ MenuRestaurante/src/Pedido.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class Pedido {
+    private int id;
+    private ArrayList<ItemMenu> itens;
+
+    public Pedido(int id) {
+        this.id = id;
+        this.itens = new ArrayList<>();
+    }
+
+    public void adicionarItem(ItemMenu item) {
+        itens.add(item);
+        System.out.println("Item "+ item + "adicionado.");
+    }
+
+    public double calcularValorTotal() {
+        double total = 0;
+        for (ItemMenu item : itens) {
+            total += item.getPreco();
+        }
+        return total;
+    }
+
+    public void descreverPedido() {
+        System.out.println("Pedido ID: " + id);
+        for (ItemMenu item : itens) {
+            item.descrever();
+        }
+        System.out.println("Valor Total: R$ " + calcularValorTotal());
+    }
+}
